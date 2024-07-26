6998cd28d6ad00ae88eb8426259b90c5e509ef8f
yangxin <1393003255@qq.com>
2024-07-26 22:59:38 +0800

    feat: 多主机执行指令函数封装

---
+++ shell/course/advance/deploy_kafka.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 初始化变量
+HOST_LIST="192.168.1.107"
+CMD_NUM=0
+
+# 多主机执行指令函数封装
+function remote_execute {
+    for host in $HOST_LIST; do
+        CMD_NUM=$((CMD_NUM + 1))
+        CMD_STR="cmd-num($CMD_NUM)"
+
+        echo "$CMD_STR cmd($@) host($host)"
+        ssh -o StrictHostKeyChecking=no root@$host $@
+        if [ $? -eq 0 ]; then
+            echo "$CMD_STR cmd($@) success"
+        else
+            echo "$CMD_STR cmd($@) failed"
+        fi
+    done
+
+    echo
+}
+
+remote_execute "df -h"
+remote_execute "ls"
+
+# 关闭 firewalld 和 setlinux
+# 安装配置 jdk
+# 安装配置 zookeeper ，并启动服务
+# 安装配置 kafka ，并启动服务
