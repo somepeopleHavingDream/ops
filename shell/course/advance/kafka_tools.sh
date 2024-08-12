adae35cfb5317b82acac508325d1c97e1bc907a1
yangxin <1393003255@qq.com>
2024-08-12 22:57:18 +0800

    feat: 一键启停脚本骨架搭建

---
+++ shell/course/advance/kafka_tools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function service_start {
+
+}
+
+function service_stop {
+
+}
+
+function service_status {
+
+}
+
+case $1 in
+start)
+    ....
+    ;;
+stop)
+    ....
+    ;;
+status)
+    ....
+    ;;
+*)
+    ....
+    ;;
+esac
