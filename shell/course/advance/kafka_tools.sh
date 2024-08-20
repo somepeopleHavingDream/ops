0533f61aafa37db5a2ee798a4d8221235d04ec5e
yangxin <1393003255@qq.com>
2024-08-20 22:33:59 +0800

    feat: 一键启动多主机 Kafka 服务函数代码实现（上）

--- shell/course/advance/kafka_tools.sh
+++ shell/course/advance/kafka_tools.sh
@@ -2,9 +2,25 @@
 
 HOST_LIST="192.168.1.109"
 
-# function service_start {
-
-# }
+function service_start {
+    for host in $HOST_LIST; do
+        service_status $host
+        if [ $? -eq 0 ]; then
+            echo "Kafka broker in $host is already RUNNING"
+        else
+            ssh -o StrictHostKeyChecking=no $host "/opt/source/kafka/bin/kafka-server-start.sh -daemon /opt/source/kafka/config/server.properties" &>/dev/null
+            index=0
+            while [ $index -le 10 ]; do
+                service_status $host
+                if [ $? -ne 0 ]; then
+                    ....
+                else
+                    ....
+                fi
+            done
+        fi
+    done
+}
 
 # function service_stop {
 
