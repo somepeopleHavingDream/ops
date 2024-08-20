9a890fe14532d18e805eac73f0c21b9bb1041f03
yangxin <1393003255@qq.com>
2024-08-20 22:23:53 +0800

    feat: 一键检测多主机 Kafka 服务状态函数代码实现

--- shell/course/advance/kafka_tools.sh
+++ shell/course/advance/kafka_tools.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-function service_start {
+HOST_LIST="192.168.1.109"
 
-}
+# function service_start {
 
-function service_stop {
+# }
 
-}
+# function service_stop {
+
+# }
 
 function service_status {
-
+    ssh -o StrictHostKeyChecking=no $1 "jps | grep -w Kafka" &>/dev/null
+    if [ $? -eq 0 ]; then
+        return
+    fi
+    return 99
 }
 
 case $1 in
-start)
-    ....
-    ;;
-stop)
-    ....
-    ;;
+start) ;;
+stop) ;;
 status)
-    ....
+    for host in $HOST_LIST; do
+        service_status $host
+        if [ $? -eq 0 ]; then
+            echo "Kafka broker in $host is RUNNING"
+        else
+            echo "Kafka broker in $host is STOPPED"
+        fi
+    done
     ;;
-*)
-    ....
-    ;;
+*) ;;
 esac
